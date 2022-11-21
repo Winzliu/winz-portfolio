fed6636c08ff83bdb0e674b803b50c37de65f9f9
Winzliu <alwin.liufandy@gmail.com>
2022-11-21 16:49:29 +0700

    membuat about dan project session

--- script.js
+++ script.js
@@ -3,13 +3,29 @@ const li = document.querySelectorAll('nav ul li ion-icon')
 
 li.forEach(el => {
   el.addEventListener('click',function(e){
-    e.target.classList.add('w-32')
+    
+    li.forEach(a => {
+      if(a.classList.contains('w-[5rem]')){
+        a.classList.remove('w-[5rem]')
+        a.classList.add('p-4')
+        a.classList.remove('pl-1')
+        a.classList.remove('py-4')
+        a.classList.remove('pr-16')
+        a.classList.add('bg-slate-200')
+        a.classList.remove('bg-cyan-500')
+        a.parentElement.nextSibling.nextSibling.classList.add('hidden')
+      }
+    })
+    
+    e.target.classList.add('w-[5rem]')
     e.target.classList.remove('p-4')
     e.target.classList.add('pl-1')
     e.target.classList.add('py-4')
-    e.target.classList.add('pr-20')
-    e.target.nextSibling.nextSibling.classList.remove('hidden')
-    // console.log(e.target.nextSibling.nextSibling)
+    e.target.classList.add('pr-16')
+    e.target.classList.remove('bg-slate-200')
+    e.target.classList.add('bg-cyan-500')
+    e.target.parentElement.nextSibling.nextSibling.classList.remove('hidden')
+
   })
 });
 
